cfd9d0499936846512be3320982ddfd67dd1c902
Kim Hayeon <k.hayeon00@gmail.com>
2023-06-06 17:04:19 +0900

    docs: 이미지 파일 업로드

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import GlobalStyle from './styles/GlobalStyle'
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 export default App;
