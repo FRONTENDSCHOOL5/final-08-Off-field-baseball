7cdc56ae8fd605eeb214eaadff94e8f90524336a
Kim Hayeon <k.hayeon00@gmail.com>
2023-06-07 16:47:32 +0900

    fix: 오타 수정

--- src/styles/CommonIcons.jsx
+++ src/styles/CommonIcons.jsx
@@ -1,9 +1,59 @@
-import ERROR_404 from '../assets/images/404.png';
-import FULL_LOGO from '../assets/images/full-logo.png';
-import SYMBOL_LOGO_GRAY from '../assets/images/symbol-logo-gray.png';
+import ARROW_LEFT from '../assets/icons/arrow-left.png';
+import X from '../assets/icons/x.png';
+import HOME from '../assets/icons/home.png';
+import HEART from '../assets/icons/heart.png';
+import EDIT from '../assets/icons/edit.png';
+import KAKAO from '../assets/icons/kakao-logo.png';
+import FACEBOOK from '../assets/icons/facebook-logo.png';
+import GOOGLE from '../assets/icons/google-logo.png';
+import USER_ICON from '../assets/icons/icon-user.png';
+import SEARCH from '../assets/icons/search.png';
+import SHARE from '../assets/icons/share.png';
+import UPLOAD_FILE from '../assets/icons/upload-file.png';
+import MORE_VERTICAL_LIGHT from '../assets/icons/more-vertical-light.png';
+import MORE_VERTICAL from '../assets/icons/more-vertical.png';
+import POST_ALBUM_ON from '../assets/icons/post-album-on.png';
+import POST_ALBUM_OFF from '../assets/icons/post-album-off.png';
+import POST_LIST_ON from '../assets/icons/post-list-on.png';
+import POST_LIST_OFF from '../assets/icons/post-list-off.png';
+import USER_FILL from '../assets/icons/user-fill.png';
+import IMG_BUTTON from '../assets/icons/img-button.png';
+import IMG_LAYERS from '../assets/icons/img-layers.png';
+import MESSAGE_CIRCLE_FILL from '../assets/icons/message-circle-fill.png';
+import MESSAGE_CIRCLE_LG from '../assets/icons/message-circle-lg.png';
+import MESSAGE_CIRCLE_SM from '../assets/icons/message-circle-sm.png';
+import IMAGE from '../assets/icons/image.png';
+import HOME_FILL from '../assets/icons/home-fill.png';
+import BASIC_PROFILE_LG from '../assets/icons/basic-profile-lg.png';
+import BASIC_PROFILE_SM from '../assets/icons/basic-profile-sm.png';
 
 export {
-  ERROR_404,
-  FULL_LOGO,
-  SYMBOL_LOGO_GRAY
+  ARROW_LEFT,
+  CLOSE,
+  HOME,
+  HEART,
+  EDIT,
+  KAKAO,
+  FACEBOOK,
+  GOOGLE,
+  USER_ICON,
+  SEARCH,
+  SHARE,
+  UPLOAD_FILE,
+  MORE_VERTICAL_LIGHT,
+  MORE_VERTICAL,
+  POST_ALBUM_ON,
+  POST_ALBUM_OFF,
+  POST_LIST_ON,
+  POST_LIST_OFF,
+  USER_FILL,
+  IMG_BUTTON,
+  IMG_LAYERS,
+  MESSAGE_CIRCLE_FILL,
+  MESSAGE_CIRCLE_LG,
+  MESSAGE_CIRCLE_SM,
+  IMAGE,
+  HOME_FILL,
+  BASIC_PROFILE_LG,
+  BASIC_PROFILE_SM,
 };
