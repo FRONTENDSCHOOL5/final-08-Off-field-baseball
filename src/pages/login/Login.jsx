35f8e7cec9d6415195ea4c500840472adf201bf5
Astinaus <slaayers0@gmail.com>
2023-06-08 18:06:01 +0900

    docs: 폴더명 PascalCase로 수정 및 import 링크 수정

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { Link } from "react-router-dom";
-import Form from '../../components/common/form/Form';
+import { Link } from 'react-router-dom';
+import Form from '../../components/common/Form/Form';
 import MainLayout from '../../components/layout/MainLayout';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 // MainLayout 확장
 const StyledLogin = styled(MainLayout)`
@@ -16,24 +16,24 @@ const StyledLogin = styled(MainLayout)`
     text-align: center;
   }
   a {
-  display: block;
-  text-align: center;
-  font-size: 1.2rem;
-  color: var(--gray-400);
+    display: block;
+    text-align: center;
+    font-size: 1.2rem;
+    color: var(--gray-400);
   }
-`
+`;
 
-function Login({team}) {
+function Login({ team }) {
   return (
     <StyledLogin>
       <h1>로그인</h1>
       <Form team={team}>
-        <label htmlFor="email-inp">이메일</label>
-        <input id="email-inp" type="text"/>
-        <label htmlFor="password-inp">비밀번호</label>
-        <input id="password-inp" type="text"/>
-        <button type="submit">로그인</button>
-        <Link to="/login">이메일로 회원가입</Link>
+        <label htmlFor='email-inp'>이메일</label>
+        <input id='email-inp' type='text' />
+        <label htmlFor='password-inp'>비밀번호</label>
+        <input id='password-inp' type='text' />
+        <button type='submit'>로그인</button>
+        <Link to='/login'>이메일로 회원가입</Link>
       </Form>
     </StyledLogin>
   );
