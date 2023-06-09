49594812df1fc6891d10f76022a0faaff74870c9
yeji.choi <moojinkl@gmail.com>
2023-06-09 11:19:42 +0900

    Style: TabNavBar 컴포넌트 중앙 정렬 추가

--- src/components/common/TabNavBar/TabNav.jsx
+++ src/components/common/TabNavBar/TabNav.jsx
@@ -47,9 +47,9 @@ const TabNavBar = styled.footer`
   height: 60px;
   position: fixed;
   bottom: 0;
-  z-index: 100;
   left: 50%;
   transform: translate(-50%);
+  z-index: 100;
   background-color: #fff;
   border-top: 1px solid #dbdbdb;
 `;
