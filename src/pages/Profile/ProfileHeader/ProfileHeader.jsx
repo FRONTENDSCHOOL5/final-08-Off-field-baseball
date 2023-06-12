ead7fdb8e1f0b6cac2613d6aef058d0626d211ef
Astinaus <slaayers0@gmail.com>
2023-06-12 09:35:20 +0900

    Style: 유저 프로필 페이지 스타일 임시 작성

--- src/pages/Profile/ProfileHeader/ProfileHeader.jsx
+++ src/pages/Profile/ProfileHeader/ProfileHeader.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import UserBtns from './UserBtns';
 import MyBtns from './MyBtns';
+import { BASIC_PROFILE_LG } from '../../../styles/CommonIcons';
 
 export default function ProfileHeader() {
   return (
     <ProfileHeaderWrapper>
+      <UserHeader>
+        <Follow>
+          <strong>2950</strong>
+          <p>Followers</p>
+        </Follow>
+        <img src={BASIC_PROFILE_LG} alt='유저 프로필 이미지' />
+        <Follow followings>
+          <strong>115</strong>
+          <p>Followings</p>
+        </Follow>
+      </UserHeader>
+      <MyTeamShow />
       {/* <UserBtns /> */}
       <MyBtns />
     </ProfileHeaderWrapper>
@@ -16,3 +29,32 @@ const ProfileHeaderWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+const UserHeader = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  img {
+    width: 110px;
+    aspect-ratio: 1 / 1;
+  }
+`;
+
+const Follow = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+
+  strong {
+    font-size: 1.8rem;
+    font-weight: bold;
+  }
+
+  p {
+    color: var(--gray-400);
+  }
+  ${({ followings }) => followings && `strong { color: var(--gray-400)}`};
+`;
+
+const MyTeamShow = styled.div``;
