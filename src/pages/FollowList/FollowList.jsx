13515a0380b61bf3bc0493a6d53d03417646e66e
Astinaus <slaayers0@gmail.com>
2023-06-12 14:45:48 +0900

    Style: 팔로우/팔로잉 리스트 페이지 작업 시작

---
+++ src/pages/FollowList/FollowList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FollowList() {
+  return <div>FollowList</div>;
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import JoinProfile from '../pages/Join/JoinProfile';
 import Feed from '../pages/Feed/Feed';
+import FollowList from '../pages/FollowList/FollowList';
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
         <Route path='/' element={<Feed />} />
         <Route path='/login' element={<Login />} />
         <Route path='/joinprofile' element={<JoinProfile />} />
+        <Route path='/user/:id/follow' element={<FollowList />} />
       </Routes>
     </BrowserRouter>
   );
