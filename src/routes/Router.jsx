c99449e78b61ea1a0479cbe3f022e009f16d2a32
Kim Hayeon <k.hayeon00@gmail.com>
2023-06-12 20:22:10 +0900

    feat: upload, post 페이지 작업중

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "../pages/Login/Login";
-import Join from "../pages/Join/Join";
-import JoinProfile from "../pages/Join/JoinProfile";
-import Upload from "../pages/Upload/Upload";
-import Feed from "../pages/Feed/Feed";
-import Post from "../pages/Post/Post";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login/Login';
+import Join from '../pages/Join/Join';
+import JoinProfile from '../pages/Join/JoinProfile';
+import Upload from '../pages/Upload/Upload';
+import Feed from '../pages/Feed/Feed';
+import Post from '../pages/Upload/Post';
+import Test from '../pages/Upload/Test';
 
 export default function Router() {
   return (
@@ -16,6 +17,7 @@ export default function Router() {
         <Route path="/join/profile" element={<JoinProfile />} />
         <Route path="/upload" element={<Upload />} />
         <Route path="/post/*" element={<Post />} />
+        <Route path="/test/*" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
