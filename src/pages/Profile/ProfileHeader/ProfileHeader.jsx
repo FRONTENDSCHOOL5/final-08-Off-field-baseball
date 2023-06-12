00ac6b7c823a6ec13c72b4c653d6b7f5f36b5372
Astinaus <slaayers0@gmail.com>
2023-06-12 10:41:35 +0900

    Style: 유저 프로필 페이지의 판매중인 상품 UI 스타일 완료

--- src/pages/Profile/ProfileHeader/ProfileHeader.jsx
+++ src/pages/Profile/ProfileHeader/ProfileHeader.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import UserBtns from './UserBtns';
 import MyBtns from './MyBtns';
 import { BASIC_PROFILE_LG } from '../../../styles/CommonIcons';
+import { GIANTS } from '../../../styles/CommonImages';
 
 export default function ProfileHeader() {
   return (
@@ -18,7 +19,17 @@ export default function ProfileHeader() {
           <p>Followings</p>
         </Follow>
       </UserHeader>
-      <MyTeamShow />
+      <MyTeamShow>
+        <img src={GIANTS} srcSet='' alt='내가 응원하는 팀' />
+        <span>롯데 자이언츠 응원중!</span>
+      </MyTeamShow>
+      <UserInfo>
+        <h2>최강롯데</h2>
+        <p className='id'>@Unbeatable_Lotte</p>
+        <p className='text'>
+          롯데 자이언츠 팬입니다. 여긴 자기 소개가 들어가요
+        </p>
+      </UserInfo>
       {/* <UserBtns /> */}
       <MyBtns />
     </ProfileHeaderWrapper>
@@ -28,16 +39,19 @@ export default function ProfileHeader() {
 const ProfileHeaderWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  padding-bottom: 24px;
+  margin-bottom: 6px;
 `;
 
 const UserHeader = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
   img {
     width: 110px;
     aspect-ratio: 1 / 1;
   }
+  margin-bottom: 8px;
 `;
 
 const Follow = styled.div`
@@ -57,4 +71,47 @@ const Follow = styled.div`
   ${({ followings }) => followings && `strong { color: var(--gray-400)}`};
 `;
 
-const MyTeamShow = styled.div``;
+const MyTeamShow = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid var(--gray-300);
+  border-radius: 10px;
+  padding: 4px;
+  width: 150px;
+  margin: 0 auto;
+  justify-content: center;
+  gap: 4px;
+  cursor: default;
+  img {
+    width: 20px;
+  }
+
+  span {
+    color: var(--gray-400);
+  }
+
+  margin-bottom: 9px;
+`;
+
+const UserInfo = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  margin-bottom: 24px;
+  h2 {
+    font-size: 1.6rem;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+  p {
+    color: var(--gray-400);
+  }
+
+  p.id {
+    font-size: 1.2rem;
+    margin-bottom: 16px;
+  }
+
+  p.text {
+    font-size: 1.4rem;
+  }
+`;
