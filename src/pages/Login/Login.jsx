1a7d0943501d53f4f7ef8f5f0f03c425621829ad
Kim Hayeon <k.hayeon00@gmail.com>
2023-06-14 15:53:19 +0900

    style: 공백 추가

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -17,16 +17,16 @@ export default function Login({ team }) {
       <h1>로그인</h1>
       <Form team={team}>
         <label htmlFor='email-inp'>이메일</label>
-        <input id='email-inp' type='text' />
+        <input id='email-inp' type='email' />
         <label htmlFor='password-inp'>비밀번호</label>
-        <input id='password-inp' type='text' />
+        <input id='password-inp' type='password' />
         <Link to='/'>
           <StyledButton
             type='submit'
             bgColor={
               isValid ? 'var(--primary-color)' : 'var(--secondary-color)'
             }
-            padding='13px 0'
+            lBtn
             disabled={isValid ? '' : 'disabled'}
           >
             로그인
