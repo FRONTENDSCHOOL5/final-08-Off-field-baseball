c948791aef6fb9be12cae8b4aa77de130b785eea
eunchong <suicas@naver.com>
2023-06-18 22:38:17 +0900

    Style: ChatList 컨텐츠 수정, 포인터 영역 수정

--- src/pages/ChatPage/ChatList/ChatList.jsx
+++ src/pages/ChatPage/ChatList/ChatList.jsx
@@ -13,22 +13,22 @@ const ChatList = () => {
       <TopBasicNav />
       <ContentsLayout>
         <ChatListStyle
-          userName='애월읍 위니브 감귤농장'
-          lastChat='이번엔 정정 언제하맨마씸?'
+          userName='최강롯데'
+          lastChat='사진을 보냈습니다.'
           date='2023.06.08'
           isNew={true}
           navigate='/chat/user1'
         />
         <ChatListStyle
-          userName='제주감귤마을'
-          lastChat='깊은 어둠의 존재감, 롤스로이스 뉴 블랙 배지'
+          userName='MSG랜더스'
+          lastChat='재밌게 보고 와~'
           date='2023.06.12'
           isNew={true}
           navigate='/chat/user2'
         />
         <ChatListStyle
-          userName='누구네 농장 친환경 한라봉'
-          lastChat='내 차는 내가 평가한다. 오픈 이벤트에 참여합니다.'
+          userName='여기한화하나요'
+          lastChat='네 그럼 내일 뵐게요!!'
           date='2023.06.15'
           navigate='/chat/user3'
         />
