4595d3064c6f979e3ecd9db01f36cd0d706e8e7f
yeji.choi <moojinkl@gmail.com>
2023-06-19 11:53:39 +0900

    Feat: 상품 등록 페이지 기능 및 라우터 추가

--- src/pages/product/ProductUpload.jsx
+++ src/pages/product/ProductUpload.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopUploadNav from '../../components/common/TopNavBar/TopUploadNav';
 import styled from 'styled-components';
-import ERROR_404 from '../../assets/images/404.png';
 import IMG_BUTTON from '../../assets/icons/img-button.png';
 
 export default function ProductUpload() {
   const [price, setPrice] = useState('');
-  const [imgPre, setImgPre] = useState(ERROR_404);
+  const [imgPre, setImgPre] = useState(null);
+  const [isValid, setIsValid] = useState(false);
+  const [productName, setProductName] = useState('');
+  const [productInfo, setProductInfo] = useState('');
+
+  useEffect(() => {
+    if (productName && price && productInfo && imgPre) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [productName, price, productInfo, imgPre]);
 
   // '가격'에 숫자만 입력 && 세자리 마다 콤마 입력
   const addComma = (price) => {
@@ -19,23 +29,37 @@ export default function ProductUpload() {
     const selectedImage = event.target.files[0];
 
     // 이미지 미리보기 생성
+    // ※ 업로드 하지않고 팝업창을 껐을 때 오류가 납니다..
     const reader = new FileReader();
     reader.onload = () => {
-      if (reader.readyState === 2) {
+      if (reader.readyState === FileReader.DONE) {
         setImgPre(reader.result);
       }
     };
     reader.readAsDataURL(selectedImage);
   };
 
+  const ResizeHeight = (e) => {
+    e.target.style.height = 'auto';
+    e.target.style.height = e.target.scrollHeight + 'px';
+  };
+
   return (
     <>
-      <TopUploadNav />
+      <TopUploadNav isValid={isValid} />
       <ProductInfo>
         <span>이미지 등록</span>
         <EmptyImg>
           {/* 임시 이미지 */}
-          <img src={imgPre} alt='' />
+          {imgPre && (
+            <img
+              style={{
+                objectFit: 'cover',
+              }}
+              src={imgPre}
+              alt=''
+            />
+          )}
 
           <label htmlFor='productImg'>
             <img src={IMG_BUTTON} id='uploadBtn' alt='' />
@@ -54,10 +78,12 @@ export default function ProductUpload() {
           <input
             type='text'
             id='name'
-            minlength='2'
-            maxlength='15'
+            minLength='2'
+            maxLength='15'
             placeholder='2~15자 이내여야 합니다.'
             required
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
           />
 
           {/* '숫자만 입력 가능합니다' 경고창 추가
@@ -77,6 +103,12 @@ export default function ProductUpload() {
           <textarea
             id='info'
             placeholder='판매하는 상품 정보를 입력해주세요.'
+            required
+            value={productInfo}
+            onChange={(e) => {
+              ResizeHeight(e);
+              setProductInfo(e.target.value);
+            }}
           />
         </ProductInput>
       </ProductInfo>
@@ -96,7 +128,7 @@ const ProductInfo = styled.section`
 
 const EmptyImg = styled.div`
   max-width: 362px;
-  height: 244px;
+  aspect-ratio: 1 / 1;
   background-color: var(--gray-100);
   margin-bottom: 30px;
   border: 0.5px solid var(--gray-200);
@@ -104,7 +136,7 @@ const EmptyImg = styled.div`
   position: relative;
 
   img {
-    object-fit: cover;
+    object-fit: conver;
     border-radius: 10px;
   }
 
@@ -145,11 +177,12 @@ const ProductInput = styled.div`
   }
 
   textarea {
-    height: 10rem;
+    min-height: 10rem;
     border: 1px solid var(--gray-200);
     border-radius: 3px;
     resize: none;
     padding: 2px;
+    overflow: hidden;
   }
 
   input:focus {
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,7 +5,7 @@ import JoinProfile from '../pages/Join/JoinProfile';
 import Upload from '../pages/Upload/Upload';
 import Feed from '../pages/Feed/Feed';
 import Profile from '../pages/Profile/Profile';
-import ProductUpload from '../pages/product/ProductUpload';
+import ProductUpload from '../pages/Product/ProductUpload';
 import Error404 from '../pages/Error404/Error404';
 import ChatList from '../pages/ChatPage/ChatList/ChatList';
 import ChatRoom1 from '../pages/ChatPage/ChatRoom/ChatRoom1';
@@ -28,8 +28,10 @@ export default function Router() {
         <Route path='/profile' element={<Profile />} />
         <Route path='/profile/:accountname' element={<Profile />} />
         <Route path='/profile/:accountname/:type' element={<FollowList />} />
-        <Route path='/productupload' element={<ProductUpload />} />
+
+        <Route path='/product/upload' element={<ProductUpload />} />
         <Route path='/search' element={<Search />} />
+
         <Route path='/error404' element={<Error404 />} />
         <Route path='/chat' element={<ChatList />} />
         <Route path='/chat/user1' element={<ChatRoom1 />} />
