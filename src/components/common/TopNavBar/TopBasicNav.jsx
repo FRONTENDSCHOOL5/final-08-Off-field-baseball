16911e797333af26604089fe3b370fbc7a7ebd08
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-06-19 10:07:07 +0900

    refactor: 메뉴 리스트 children으로 넘김

--- src/components/common/TopNavBar/TopBasicNav.jsx
+++ src/components/common/TopNavBar/TopBasicNav.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TopNavBar, LeftArrow, MoreBtn } from './Styled';
 import { ARROW_LEFT, MORE_VERTICAL } from '../../../styles/CommonIcons';
 import styled from 'styled-components';
-import MoreModal from '../Modal/SettingModal';
+import MoreModal from '../Modal/MoreModal';
 import { useNavigate } from 'react-router-dom';
 
 export default function TopBasicNav({ accountname = '' }) {
@@ -21,7 +21,14 @@ export default function TopBasicNav({ accountname = '' }) {
         </MoreBtn>
       </TopNavBar>
       {isModalOpen && (
-        <MoreModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+        <MoreModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+          <li>
+            <button type='button'>설정 및 개인정보</button>
+          </li>
+          <li>
+            <button type='button'>로그아웃</button>
+          </li>
+        </MoreModal>
       )}
     </>
   );
