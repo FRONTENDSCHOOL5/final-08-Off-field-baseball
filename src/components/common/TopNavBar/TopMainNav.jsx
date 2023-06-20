4220ba0a6a048d4fd6076c46d1e6c402b5a7779f
Astinaus <slaayers0@gmail.com>
2023-06-20 13:23:34 +0900

    feat: 게시글/피드 무한 스크롤 기능 구현

--- src/components/common/TopNavBar/TopMainNav.jsx
+++ src/components/common/TopNavBar/TopMainNav.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { TopNavBar, SearchBtn } from './Styled';
 import { SEARCH } from '../../../styles/CommonIcons';
+import { useNavigate } from 'react-router-dom';
 
 export default function TopMainNav() {
+  const navigate = useNavigate();
   return (
     <TopNavBar>
       <TopNavH1>구장 밖 야구 피드</TopNavH1>
-      <SearchBtn>
+      <SearchBtn onClick={() => navigate('/search')}>
         <img src={SEARCH} alt='검색 창 열기 버튼' />
       </SearchBtn>
     </TopNavBar>
