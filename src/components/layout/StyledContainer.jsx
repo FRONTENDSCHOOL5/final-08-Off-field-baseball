8fa32ecd051680337f778bfb25f1ec75bd2e891f
Kim Hayeon <k.hayeon00@gmail.com>
2023-06-25 18:01:42 +0900

    fix: container 요소 div로 변경(main 중복 사용 수정)

--- src/components/layout/StyledContainer.jsx
+++ src/components/layout/StyledContainer.jsx
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 
-export default function MainLayout({ children }) {
-  return <MainWrapper>{children}</MainWrapper>;
-}
-
-const MainWrapper = styled.main`
+const StyledContainer = styled.div`
   width: min(100%, 430px);
   min-height: 100vh;
   box-shadow: 0px 0px 4px var(--gray-300); /* 임시 */
   background: white;
   margin: auto;
 `;
+
+export default StyledContainer;
