09270be80f0a97994501ec84507102a5bbbf3c86
Astinaus <slaayers0@gmail.com>
2023-06-26 10:04:23 +0900

    Feat: Router.jsx 에 Suspense, lazy 적용

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,63 +1,70 @@
+import React, { Suspense, lazy } from 'react';
 import AuthRoute from './AuthRoute';
 import NonAuthRoute from './NonAuthRoute';
 import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login/Login';
-import Join from '../pages/Join/Join';
-import Upload from '../pages/Upload/Upload';
-import Feed from '../pages/Feed/Feed';
-import Profile from '../pages/Profile/Profile';
-import ProfileEdit from '../pages/Profile/ProfileEdit';
-import ProductUpload from '../pages/Product/ProductUpload';
-import Error404 from '../pages/Error404/Error404';
-import ChatList from '../pages/ChatPage/ChatList/ChatList';
-import ChatRoom1 from '../pages/ChatPage/ChatRoom/ChatRoom1';
-import ChatRoom2 from '../pages/ChatPage/ChatRoom/ChatRoom2';
-import ChatRoom3 from '../pages/ChatPage/ChatRoom/ChatRoom3';
-import FollowList from '../pages/FollowList/FollowList';
-import Search from '../pages/Search/Search';
-import Detail from '../pages/Upload/Detail';
-import SplashScreen from '../pages/SplashScreen/SplashScreen';
-import SplashLogin from '../pages/Login/SplashLogin';
-import ProductDetail from '../pages/Product/ProductDetail';
+import Loading from '../components/common/Loading';
+const Login = lazy(() => import('../pages/Login/Login'));
+const Join = lazy(() => import('../pages/Join/Join'));
+const Upload = lazy(() => import('../pages/Upload/Upload'));
+const Feed = lazy(() => import('../pages/Feed/Feed'));
+const Profile = lazy(() => import('../pages/Profile/Profile'));
+const ProfileEdit = lazy(() => import('../pages/Profile/ProfileEdit'));
+const ProductUpload = lazy(() => import('../pages/Product/ProductUpload'));
+const Error404 = lazy(() => import('../pages/Error404/Error404'));
+const ChatList = lazy(() => import('../pages/ChatPage/ChatList/ChatList'));
+const ChatRoom1 = lazy(() => import('../pages/ChatPage/ChatRoom/ChatRoom1'));
+const ChatRoom2 = lazy(() => import('../pages/ChatPage/ChatRoom/ChatRoom2'));
+const ChatRoom3 = lazy(() => import('../pages/ChatPage/ChatRoom/ChatRoom3'));
+const Search = lazy(() => import('../pages/Search/Search'));
+const FollowList = lazy(() => import('../pages/FollowList/FollowList'));
+const Detail = lazy(() => import('../pages/Upload/Detail'));
+const SplashScreen = lazy(() => import('../pages/SplashScreen/SplashScreen'));
+const SplashLogin = lazy(() => import('../pages/Login/SplashLogin'));
+const ProductDetail = lazy(() => import('../pages/Product/ProductDetail'));
 
 export default function Router() {
   const { token } = useContext(UserContext);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='*' element={<Error404 />} />
-        <Route path='/error404' element={<Error404 />} />
-        <Route path='/' element={<SplashScreen />} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path='*' element={<Error404 />} />
+          <Route path='/error404' element={<Error404 />} />
+          <Route path='/' element={<SplashScreen />} />
 
-        <Route element={<NonAuthRoute authenticated={token} />}>
-          <Route path='/login' element={<SplashLogin />} />
-          <Route path='/login/email' element={<Login />} />
-          <Route path='/join' element={<Join />} />
-        </Route>
+          <Route element={<NonAuthRoute authenticated={token} />}>
+            <Route path='/login' element={<SplashLogin />} />
+            <Route path='/login/email' element={<Login />} />
+            <Route path='/join' element={<Join />} />
+          </Route>
 
-        <Route element={<AuthRoute authenticated={token} />}>
-          <Route path='/home' element={<Feed />} />
-          <Route path='/post/upload' element={<Upload />} />
-          <Route path='/post/:id' element={<Detail />} />
-          <Route path='/post/:id/edit' element={<Upload />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/profile/:username' element={<Profile />} />
-          <Route path='/profile/edit' element={<ProfileEdit />} />
-          <Route path='/profile/:accountname/:type' element={<FollowList />} />
-          <Route path='/product/upload' element={<ProductUpload />} />
-          <Route path='/product/:id' element={<ProductDetail />} />
-          <Route path='/product/:id/edit' element={<ProductUpload />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/chat' element={<ChatList />} />
-          <Route path='/chat/user1' element={<ChatRoom1 />} />
-          <Route path='/chat/user2' element={<ChatRoom2 />} />
-          <Route path='/chat/user3' element={<ChatRoom3 />} />
-          <Route path='/follow' element={<FollowList />} />
-        </Route>
-      </Routes>
+          <Route element={<AuthRoute authenticated={token} />}>
+            <Route path='/home' element={<Feed />} />
+            <Route path='/post/upload' element={<Upload />} />
+            <Route path='/post/:id' element={<Detail />} />
+            <Route path='/post/:id/edit' element={<Upload />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/profile/:username' element={<Profile />} />
+            <Route path='/profile/edit' element={<ProfileEdit />} />
+            <Route
+              path='/profile/:accountname/:type'
+              element={<FollowList />}
+            />
+            <Route path='/product/upload' element={<ProductUpload />} />
+            <Route path='/product/:id' element={<ProductDetail />} />
+            <Route path='/product/:id/edit' element={<ProductUpload />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/chat' element={<ChatList />} />
+            <Route path='/chat/user1' element={<ChatRoom1 />} />
+            <Route path='/chat/user2' element={<ChatRoom2 />} />
+            <Route path='/chat/user3' element={<ChatRoom3 />} />
+            <Route path='/follow' element={<FollowList />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
